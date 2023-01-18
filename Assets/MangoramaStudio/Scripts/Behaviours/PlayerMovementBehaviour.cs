2df9abfc69a78eb9f15ce677c7dccc476b188094
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-18 15:35:41 +0300

    player movement behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _playerSpeed;
+
+    private void Start()
+    {
+        InputController.OnDrag += Dragged;
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(0, 0, _playerSpeed) * Time.deltaTime;
+    }
+
+    private void Dragged(Vector2 dragVector)
+    {
+        transform.position += new Vector3(dragVector.x * 100f, 0, 0) * Time.deltaTime;
+    }
+}
