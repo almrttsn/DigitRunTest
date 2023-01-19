3548bb140b896db00b6330ecdfbd7408ee9182fe
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-19 15:50:39 +0300

    player controller system created

---
+++ Assets/MangoramaStudio/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerDetectorBehaviour PlayerDetectorBehaviour => _playerDetectorBehaviour;
+    public PlayerFireBehaviour PlayerFireBehaviour => _playerFireBehaviour;
+    public PlayerMoneyDisplayBehaviour PlayerMoneyDisplayBehaviour => _playerMoneyDisplayBehaviour;
+    public PlayerMovementBehaviour PlayerMovementBehaviour => _playerMovementBehaviour;
+
+    [SerializeField] private PlayerDetectorBehaviour _playerDetectorBehaviour;
+    [SerializeField] private PlayerFireBehaviour _playerFireBehaviour;
+    [SerializeField] private PlayerMoneyDisplayBehaviour _playerMoneyDisplayBehaviour;
+    [SerializeField] private PlayerMovementBehaviour _playerMovementBehaviour;
+
+    private void Start()
+    {
+        PlayerDetectorBehaviour.Initialize(this);
+        PlayerFireBehaviour.Initialize(this);
+        PlayerMoneyDisplayBehaviour.Initialize(this);
+        PlayerMovementBehaviour.Initialize(this);
+    }
+}
