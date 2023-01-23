72cbbebf6d2f941776ec661ca3418a19eb4d3c53
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-23 16:11:58 +0300

    transform clamping process

--- Assets/MangoramaStudio/Scripts/Behaviours/PlayerMovementBehaviour.cs
+++ Assets/MangoramaStudio/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -17,6 +17,7 @@ public class PlayerMovementBehaviour : MonoBehaviour
     private void Update()
     {
         transform.position += new Vector3(0, 0, _playerSpeed) * Time.deltaTime;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.5f, 2.5f), transform.position.y, transform.position.z);
     }
 
     private void Dragged(Vector2 dragVector)
