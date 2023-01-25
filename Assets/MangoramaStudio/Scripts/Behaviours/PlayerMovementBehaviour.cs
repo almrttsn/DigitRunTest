0e7c02f635a272235508063656323c0f30e0fb54
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-01-25 16:41:47 +0300

    player speed getter setter system

--- Assets/MangoramaStudio/Scripts/Behaviours/PlayerMovementBehaviour.cs
+++ Assets/MangoramaStudio/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -7,13 +7,14 @@ public class PlayerMovementBehaviour : MonoBehaviour
 {
     public bool PlayerMovementRestricted { get; set; }
 
-    [SerializeField] private float _playerSpeed;
+    public float PlayerSpeed { get; set; }
 
     private PlayerController _playerController;
 
     public void Initialize(PlayerController playerController)
     {
         _playerController = playerController;
+        PlayerSpeed = 7.5f;
         InputController.OnDrag += Dragged;
     }
 
@@ -21,7 +22,7 @@ public class PlayerMovementBehaviour : MonoBehaviour
     {
         if (PlayerMovementRestricted == false)
         {
-            transform.position += new Vector3(0, 0, _playerSpeed) * Time.deltaTime;
+            transform.position += new Vector3(0, 0, PlayerSpeed) * Time.deltaTime;
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.5f, 2.5f), transform.position.y, transform.position.z);
         }
     }
@@ -29,5 +30,10 @@ public class PlayerMovementBehaviour : MonoBehaviour
     private void Dragged(Vector2 dragVector)
     {
         transform.position += new Vector3(dragVector.x * 100f, 0, 0) * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        InputController.OnDrag -= Dragged;
     }
 }
